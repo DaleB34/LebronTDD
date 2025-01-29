bfb3e44217407135d4fba1cc5a303e8c03f4a740
DaleB34 <dalebolido@gmail.com>
2025-01-29 11:17:05 -0800

    Made Test quotedByBron

--- Test/LebronQuoteTest.java
+++ Test/LebronQuoteTest.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class LeBronQuoteTest {
+class LeBronQuoteTest
+{
+    @Test
+    void quotedByBron()
+    {
+        LeBronQuote q = new LeBronQuote();
+        q.setSource("LeBron James");
+        assertTrue(q.getSource().matches("(^LeBron) (James$)"));
+    }
 
 }
