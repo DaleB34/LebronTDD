4fb7a90d2d71b8e3b91e4ef74f5cda381d564d78
DaleB34 <dalebolido@gmail.com>
2025-02-03 12:48:22 -0800

    removed tests quotedByBron and notQuotedByBron

--- Test/LebronQuoteTest.java
+++ Test/LebronQuoteTest.java
@@ -5,22 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class LeBronQuoteTest
 {
     @Test
-    void quotedByBron()
-    {
-        LeBronQuote q = new LeBronQuote();
-        q.setSource("LeBron James");
-        assertTrue(q.getSource().matches("(^LeBron) (James$)"));
-    }
-
-    @Test
-    void notQuotedByBron()
-    {
-        LeBronQuote q = new LeBronQuote();
-        q.setSource("Chance The Rapper");
-        assertFalse(q.getSource().matches("(^Lebron) (James$)"));
-    }
-
-    @Test
     void logicalQuote()
     {
         LeBronQuote q = new LeBronQuote();
